ba0380ad478496ff14cd4b53cd76ff12a1912e63
Shashank Swaminathan <sh.swami235@gmail.com>
2019-06-10 06:06:27 +0000

    Fixing issue with emacs aliases

--- standardSetup/setup.sh
+++ standardSetup/setup.sh
@@ -2,4 +2,5 @@
 
 mkdir ~/.emacs.d/
 ln ../init.el ~/.emacs.d/init.el
-echo "em='emacs -q -l ~/.emacs.d/init.el'" >> ~/.bashrc
+echo "alias em='emacs -q -l ~/.emacs.d/init.el'" >> ~/.bashrc
+source ~/.bashrc
