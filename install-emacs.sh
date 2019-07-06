87e3cc60c99ba6d4a40639ceb76692c2d99795a7
Shashank Swaminathan <sh.swami235@gmail.com>
2019-07-06 09:27:12 +0000

    Adding packages to add PPAs to quick restart script

--- install-emacs.sh
+++ install-emacs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install the software common properties package to be able to add PPAs
+apt update
+apt install -y software-properties-common
+
 # Install the better version of emacs
 add-apt-repository ppa:kelleyk/emacs
 apt update
